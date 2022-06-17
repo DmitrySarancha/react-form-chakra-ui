869cca1a246af6a7cf4b9745daa2dde00d4fff4f
Dmitry Sarancha <lottyskound@gmail.com>
2022-06-17 23:19:57 +0300

    create color mode switcher

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,15 +1,17 @@
 import { Flex, Heading, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
+import { ColorModeSwitcher } from '../../../utils/ColorModeSwitcher';
 
 export const Header = () => {
 	const bg = useColorModeValue('gray.300', 'gray.600');
 	const color = useColorModeValue('gray.600', 'gray.300');
 
 	return (
-		<Flex as="header" w="full" p="4" bg={bg}>
+		<Flex as="header" w="full" p="4" bg={bg} align="center">
 			<Heading as="h1" size="2xl" flexGrow="1" textAlign="center" color={color}>
 				The Ultimate Form Challenge
 			</Heading>
+			<ColorModeSwitcher />
 		</Flex>
 	);
 };
