5d674f28f2d8f1e991ede1b7447d60ca45f343c4
Dmitry Sarancha <lottyskound@gmail.com>
2022-06-19 12:40:46 +0300

    input component: add new props

--- src/components/FormItems/Input.jsx
+++ src/components/FormItems/Input.jsx
@@ -9,8 +9,15 @@ import {
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-export const InputFormControl = ({ name, register, errors, helper = '' }) => {
-	const inputFocusBorderColor = useColorModeValue('purple.300', 'pink.400');
+export const InputFormControl = ({
+	name,
+	register,
+	errors,
+	helper = '',
+	onChange = undefined,
+	type,
+}) => {
+	const inputFocusBorderColor = useColorModeValue('purple.300', 'yellow.600');
 	const inputStyleShadow = useColorModeValue('md', 'dark-lg');
 	const boxStyleBorderColor = useColorModeValue('cyan.300', 'blue.700');
 	const boxStyleShadow = useColorModeValue('lg', 'dark-lg');
@@ -43,10 +50,13 @@ export const InputFormControl = ({ name, register, errors, helper = '' }) => {
 			<FormControl {...formControlStyle} isRequired isInvalid={errors[name]}>
 				<FormLabel htmlFor={name}>{name}</FormLabel>
 				<Input
+					type={type}
 					{...inputStyle}
 					id={name}
 					placeholder={name}
-					{...register(`${name}`)}
+					{...register(`${name}`, {
+						onChange: onChange,
+					})}
 				/>
 				{errors[name] ? (
 					<FormErrorMessage>{errors?.[name]?.message}</FormErrorMessage>
@@ -59,8 +69,10 @@ export const InputFormControl = ({ name, register, errors, helper = '' }) => {
 };
 
 InputFormControl.propTypes = {
+	onChange: PropTypes.func,
 	register: PropTypes.func.isRequired,
 	errors: PropTypes.object.isRequired,
 	name: PropTypes.string.isRequired,
 	helper: PropTypes.string.isRequired,
+	type: PropTypes.string,
 };
